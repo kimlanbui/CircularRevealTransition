51865df4cb6543621495ded63929842329646496
Kim Lan Bui <kim.l4n.bui@googlemail.com>
2017-02-07 23:07:27 +0100

    Added empty pod.

---
+++ Example/Pods/Target Support Files/Pods-CircularRevealTransition_Example/Pods-CircularRevealTransition_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CircularRevealTransition_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CircularRevealTransition_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-CircularRevealTransition_Tests/Pods-CircularRevealTransition_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_CircularRevealTransition_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CircularRevealTransition_TestsVersionString[];
+
