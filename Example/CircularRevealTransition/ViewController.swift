7bb4cc0c1d2bf4fe96e80ece555f36b77d0813ec
Kim Lan Bui <kim.l4n.bui@googlemail.com>
2017-02-16 19:47:39 +0100

    Converted to Swift 3 syntax.

--- Example/CircularRevealTransition/ViewController.swift
+++ Example/CircularRevealTransition/ViewController.swift
@@ -24,14 +24,14 @@ class ViewController : UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         transitionDelegate = CircularRevealTransitionDelegate(frame: button.frame)
-        let destinationController = segue.destinationViewController
+        let destinationController = segue.destination
         destinationController.transitioningDelegate = transitionDelegate
         // destinationController.modalPresentationStyle = UIModalPresentationStyle.FullScreen
     }
     
-    @IBAction func unwindFromView(segue: UIStoryboardSegue) {}
+    @IBAction func unwindFromView(_ segue: UIStoryboardSegue) {}
     
     deinit {
         print("remove ViewController")
@@ -42,17 +42,17 @@ class BlueViewController : UIViewController {
     
     var transitionDelegate: UIViewControllerTransitioningDelegate?
     
-    @IBAction func didTapButton(sender: UIButton!) {
+    @IBAction func didTapButton(_ sender: UIButton!) {
         transitionDelegate = CircularRevealTransitionDelegate(frame: sender.frame)
         transitioningDelegate = transitionDelegate
-        dismissViewControllerAnimated(true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
     }
     
